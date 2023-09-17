28c6913403356bf4e0ef9085bbfbe7dfb3210f7e
Alirza <ali.zaidov.18@gmail.com>
2023-09-17 14:19:01 +0400

    Done

--- products-task/src/cookie service/Cookie.js
+++ products-task/src/cookie service/Cookie.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 import Cookies from "js-cookie";
 
-export default function Cookie() {
+export default function Cookie(product) {
   const [favorites, setFavorites] = useState([]);
 
   useEffect(() => {
@@ -11,6 +11,22 @@ export default function Cookie() {
       setFavorites(JSON.parse(favoriteProducts));
     }
   }, []);
+
+  const isFavorite = favorites.some(
+    (favoriteProduct) => favoriteProduct.Title === product.Id
+  );
+  if (isFavorite) {
+    const newFavorites = favorites.filter(
+      (favoriteProduct) => favoriteProduct.Title !== product.Id
+    );
+    setFavorites(newFavorites);
+    Cookies.set("favoriteProducts", JSON.stringify(newFavorites));
+  } else {
+    const newFavorites = [...favorites, product];
+    setFavorites(newFavorites);
+    Cookies.set("favoriteProducts", JSON.stringify(newFavorites));
+    
+  }
 
   return <div></div>;
 }
